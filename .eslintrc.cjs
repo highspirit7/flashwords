94dcedc219db9bc0928abd2fd29c348d03801f15
GrownUprince <jake.lee4006@gmail.com>
2024-04-16 12:45:00 +0200

    chore: add settings in eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,18 +8,31 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-airbnb',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
   overrides: [
     {
       files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:playwright/recommended']
-    }
+      extends: ['plugin:playwright/recommended'],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   rules: {
-    'import/no-extraneous-dependencies': 'off'
-  }
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+  },
+  settings: {
+    // to make our custom @ alias resolvable by ESLint import rules
+    'import/resolver': {
+      [require.resolve('eslint-import-resolver-node')]: {},
+      [require.resolve('eslint-import-resolver-custom-alias')]: {
+        alias: {
+          '@': `${path.resolve(__dirname, './src')}`,
+        },
+        extensions: ['.mjs', '.js', '.jsx', '.json', '.node', '.ts', '.tsx'],
+      },
+    },
+  },
 }
