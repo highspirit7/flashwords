f6b0eb3fd1a29cd00ef4a60ef483eb4d9bf6db68
GrownUprince <jake.lee4006@gmail.com>
2024-04-29 17:36:10 +0200

    fix(DeleteExampleModal): close & cancel button

--- src/components/DeleteExampleModal.vue
+++ src/components/DeleteExampleModal.vue
@@ -30,6 +30,7 @@ function handleClickDelete() {
           type="button"
           class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           data-modal-hide="delete-example-modal"
+          @click="toggleDeleteExampleModal"
         >
           <svg
             class="w-3 h-3"
@@ -78,6 +79,7 @@ function handleClickDelete() {
             data-modal-hide="delete-example-modal"
             type="button"
             class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            @click="toggleDeleteExampleModal"
           >
             Cancel
           </button>
