230bb7487ec477549656a02e7283ec6c0fda44fb
GrownUprince <jake.lee4006@gmail.com>
2024-04-30 10:47:21 +0200

    style: make TopNavBar sticky

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
-import Navbar from '@/components/Navbar.vue'
+import TopNavbar from '@/components/TopNavbar.vue'
 
 // TODO : Need to use the code to extract the theme of OS later after handling styling for dark mode in all pages
 if (localStorage.getItem('color-theme') === 'dark') {
@@ -13,8 +13,8 @@ if (localStorage.getItem('color-theme') === 'dark') {
 </script>
 
 <template>
-  <header class="flex justify-center bg-white w-full">
-    <Navbar />
+  <header class="flex justify-center bg-white w-full sticky top-0 z-50">
+    <TopNavbar />
   </header>
   <main class="max-w-3xl w-full py-4">
     <RouterView />
