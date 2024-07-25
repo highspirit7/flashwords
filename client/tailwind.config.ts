a9c03e4f65ccb85dbe9c3df3169f2c1e19661f10
GrownUprince <jake.lee4006@gmail.com>
2024-07-25 08:22:47 +0200

    fix: update path of flowbite

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,14 +1,15 @@
+import flowbitePlugin from 'flowbite/plugin'
 import type { Config } from 'tailwindcss'
 
 export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+    '../node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    '../node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [flowbitePlugin],
 } satisfies Config
