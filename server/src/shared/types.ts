8c844aded4805e447e1e6eccf4dc18f1793f06da
GrownUprince <jake.lee4006@gmail.com>
2024-08-05 11:04:18 +0200

    feat(server): update entites(types, fakes) for card, cardset

--- server/src/shared/types.ts
+++ server/src/shared/types.ts
@@ -2,6 +2,6 @@
 // Types are stripped out by the compiler, so this is safe.
 export type * from '../database/types'
 
-export type * from '../entities/article'
-export type * from '../entities/comment'
+export type * from '../entities/card'
+export type * from '../entities/cardset'
 export type * from '../entities/user'
