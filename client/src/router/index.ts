50150a8178c3908503b14b734fec19d62e0380dc
GrownUprince <jake.lee4006@gmail.com>
2024-09-09 13:38:32 +0200

    feat(client): add forbidden page & apply navigation guard for cardset route

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,6 +3,9 @@ import HomeView from '@/views/HomeView.vue'
 import useAuthStore from '@/stores/auth'
 import { handleAuthenticationError } from '@/utils/auth'
 import { useToasterStore } from '@/stores/toaster'
+import { authTrpc } from '@/trpc'
+import { isString } from '@/utils/typePredicates'
+import { TRPCClientError } from '@trpc/client'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,16 +14,18 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-      beforeEnter: async () => {
+      beforeEnter: async (_to, _from, next) => {
         const authStore = useAuthStore()
         const toasterStore = useToasterStore()
         const { isLoggedIn } = authStore
 
-        if (isLoggedIn) return { name: 'cardsets' }
-        else {
+        if (isLoggedIn) {
+          next({ path: '/cardsets', replace: true })
+          return true
+        } else {
           try {
             await authStore.verifyWithRefreshToken()
-            router.replace('/cardsets')
+            next({ path: '/cardsets', replace: true })
           } catch (error) {
             handleAuthenticationError(error, toasterStore)
           }
@@ -52,20 +57,46 @@ const router = createRouter({
       path: '/:cardsetTitle/:id',
       name: 'cardset',
       component: () => import('../views/CardsetView.vue'),
+      beforeEnter: async to => {
+        if (!isString(to.params.id))
+          return { name: 'notFound', params: { pathMatch: to.path.split('/').slice(1) } }
+
+        const cardsetId = parseInt(to.params.id)
+
+        try {
+          await authTrpc.cardset.find.query(cardsetId)
+          return true
+        } catch (error: unknown) {
+          if (error instanceof TRPCClientError) {
+            if (error.data?.httpStatus === 403)
+              return {
+                name: 'forbidden',
+                params: { pathMatch: to.path.split('/').slice(1) },
+              }
+          }
+
+          return {
+            name: 'notFound',
+            params: { pathMatch: to.path.split('/').slice(1) },
+          }
+        }
+      },
     },
     {
       path: '/:cardsetTitle/:id/edit',
       name: 'editCardset',
       component: () => import('../views/CardsetEditView.vue'),
     },
+    // TODO : also need navigation guard for card route
     {
       path: '/:cardsetTitle/:cardsetId/:cardTerm/:cardId',
       name: 'card',
       component: () => import('../views/CardView.vue'),
     },
+    { path: '/forbidden', name: 'forbidden', component: () => import('../views/Forbidden.vue') },
     {
       path: '/:catchAll(.*)*',
-      name: 'NotFound',
+      name: 'notFound',
       component: () => import('../views/NotFound.vue'),
     },
   ],
